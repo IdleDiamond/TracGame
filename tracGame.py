0b9cfefc773c00d3a89bd9610d3106b61d80bd80
IdleDiamond <jfdcoding@gmail.com>
2024-02-03 22:22:28 -0500

    Finished dice roll

--- tracGame.py
+++ tracGame.py
@@ -16,7 +16,8 @@
 """
  
 import pygame
- 
+from random import randint
+
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self):
@@ -33,34 +34,40 @@ class Dice(pygame.sprite.Sprite):
         dice_5 = pygame.image.load('art/dice_5.png').convert_alpha()
         dice_6 = pygame.image.load('art/dice_6.png').convert_alpha()
         
+        self.diceFace = 0
+        self.diceNumber = diceNumber
         self.frames = (dice_1, dice_2, dice_3, dice_4, dice_5, dice_6)
-        self.dice_face = 0
-        self.image = self.frames[self.dice_face]
+        self.image = self.frames[self.diceFace]
+
         
-        if diceNumber == 1:
-            self.rect = self.image.get_rect(midbottom = (80, 430))
+        if self.diceNumber == 1:
+            self.rect = self.image.get_rect(midbottom = (80, 410))
         else:
-            self.rect = self.image.get_rect(midbottom = (140, 430))
+            self.rect = self.image.get_rect(midbottom = (140, 410))
         
     def show(self):
-        self.image = self.frames[self.dice_face]
+        self.image = self.frames[self.diceFace]
         
     def roll(self):
-        #use random
-        #call animation??
-        self.dice_face = 3
+        #Result from rolling the diec
+        self.diceFace = randint(0,5)
         
     def player_event(self):
-        #if space press then roll 
         pass
     
+    def getDiceFace(self):
+        return self.diceFace + 1
+    
     def dice_animation(self):
-        #when roll event
-        pass
+        if ROLLING_DICE:
+            rolling_face = randint(0,5)
+            self.image = self.frames[rolling_face]
+        else:
+            self.show()
         
     def update(self):
         self.player_event()
-        self.show()
+        self.dice_animation()
         
 
 # Define some colors
@@ -69,17 +76,31 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
-GAME_ACTIVE = True
+GAME_ACTIVE = False
+ROLLING_DICE = False
 
- 
+
 pygame.init()
  
+text_font = pygame.font.Font(None, 80)
+
+
 # Set the width and height of the screen [width, height]
 size = (800, 500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Trac Game')
 bg_surf = pygame.image.load("art/card_table.jpg").convert_alpha()
 bg_surf = pygame.transform.smoothscale(bg_surf, size)
+
+#initial screen
+game_name = text_font.render("Trac Game", False, BLACK)
+game_name_rect = game_name.get_rect(midbottom = (400,200))
+game_msg = text_font.render("Press space to start", True, BLACK)
+game_msg_rect = game_msg.get_rect(midbottom = (400, 330))
+
+#Game screen
+roll_msg = text_font.render("Press space to roll dices", False, BLACK)
+roll_msg_rect = roll_msg.get_rect(midbottom = (400, 480))
 
 
 #Groups
@@ -93,6 +114,8 @@ done = False
  
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
+
+rollTimer = pygame.USEREVENT + 1
  
 # -------- Main Program Loop -----------
 while not done:
@@ -100,20 +123,36 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
- 
+        
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            #player press SPACE to roll dices
+            if GAME_ACTIVE:
+                pygame.time.set_timer(rollTimer, 1000)
+                ROLLING_DICE = True
+                for dice in dice_group:
+                    dice.roll()     
+            else:
+                #player started game
+                GAME_ACTIVE = True
+        
+        if event.type == rollTimer:
+            #stop rolling dices
+            ROLLING_DICE = False
+        
+    #table background
     screen.blit(bg_surf, (0,0))
  
+    #different game screens
     if GAME_ACTIVE:
+        
         dice_group.update()
         dice_group.draw(screen)
+        screen.blit(roll_msg, roll_msg_rect)
         
     else:
-        pass
+        screen.blit(game_name, game_name_rect)
+        screen.blit(game_msg, game_msg_rect)
  
-    
- 
-    
-    
  
     # --- Drawing code should go here
  
