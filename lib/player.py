8b3c84a14107652f02f288b4a5fc5c8c534c20c7
IdleDiamond <jfdcoding@gmail.com>
2024-03-28 10:28:32 -0400

    added inactive player animation

--- lib/player.py
+++ lib/player.py
@@ -10,13 +10,13 @@ Created on Fri Mar 22 21:50:13 2024
 import pygame
 
 class Player(pygame.sprite.Sprite):
-    def __inti__(self, playerNum):
+    def __init__(self, playerNum):
         super().__init__()
         self.playerNumber = playerNum
         self.score = 0
-        self.diceInPlay = 2
-        #self.image = pygame.image.load("art/player.png").convert_alpha()
-        #self.rect = self.image.get_rect(midleft = (20,50))
+        self.reducedDice = False
+        self.image = pygame.image.load(f"art/player/player{self.playerNumber}.png").convert_alpha()
+        self.rect = self.image.get_rect(midleft = (20,50))
 
     def update(self):
         pass
