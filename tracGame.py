98a73f67f82658b5f81918b8a384fde0e8ad8704
IdleDiamond <jfdcoding@gmail.com>
2024-04-02 12:14:41 -0400

    Pycharm errors on main tracGame.py

--- tracGame.py
+++ tracGame.py
@@ -21,50 +21,51 @@ from lib.card import Card
 from lib.player import Player
 
 
-def checkTurnEnd():
+def check_turn_end():
     """Checks if with dice result the game is still possible"""
 
+    POSSIBILITY_DICT = {12: [{9, 3}, {9, 2, 1}, {8, 4}, {8, 3, 1}, {7, 5}, {7, 3, 2}, {7, 4, 1}, {6, 5, 1}, {6, 4, 2},
+                             {6, 3, 2, 1}, {5, 4, 3}],
+                        11: [{9, 2}, {8, 3}, {8, 2, 1}, {7, 4}, {7, 3, 1}, {6, 5}, {6, 4, 1}, {6, 3, 2}, {5, 4, 2},
+                             {5, 3, 2, 1}],
+                        10: [{9, 1}, {8, 2}, {7, 3}, {7, 2, 1}, {6, 4}, {6, 3, 1}, {5, 4, 1}, {5, 3, 2}, {4, 3, 2, 1}],
+                        9: [{9}, {8, 1}, {7, 2}, {6, 3}, {6, 2, 1}, {5, 4}, {5, 3, 1}, {4, 3, 2}],
+                        8: [{8}, {7, 1}, {6, 2}, {5, 3}, {5, 2, 1}, {4, 3, 1}],
+                        7: [{7}, {6, 1}, {5, 2}, {4, 3}, {4, 2, 1}],
+                        6: [{6}, {5, 1}, {4, 2}, {3, 2, 1}],
+                        5: [{5}, {4, 1}, {3, 2}],
+                        4: [{4}, {3, 1}],
+                        3: [{3}, {2, 1}],
+                        2: [{2}],
+                        1: [{1}]}
 
-    POSSIBILITY_DICT = {12: [{9,3}, {9,2,1}, {8,4}, {8,3,1}, {7,5}, {7,3,2}, {7,4,1}, {6,5,1}, {6,4,2}, {6,3,2,1}, {5,4,3}],
-                       11: [{9,2}, {8,3}, {8,2,1}, {7,4}, {7,3,1}, {6,5}, {6,4,1}, {6,3,2}, {5,4,2}, {5,3,2,1}],
-                       10: [{9,1}, {8,2}, {7,3}, {7,2,1}, {6,4}, {6,3,1}, {5,4,1}, {5,3,2}, {4,3,2,1}],
-                       9: [{9}, {8,1}, {7,2}, {6,3}, {6,2,1}, {5,4}, {5,3,1}, {4,3,2}],
-                       8: [{8}, {7,1}, {6,2}, {5,3}, {5,2,1}, {4,3,1}],
-                       7: [{7}, {6,1}, {5,2}, {4,3}, {4,2,1}],
-                       6: [{6}, {5,1}, {4,2}, {3,2,1}],
-                       5: [{5}, {4,1}, {3,2}],
-                       4: [{4}, {3,1}],
-                       3: [{3}, {2,1}],
-                       2: [{2}],
-                       1: [{1}]}
+    is_turn_impossible = True
+    pos_list = POSSIBILITY_DICT[diceResult]
 
-    isTurnImpossible = True
-    posList = POSSIBILITY_DICT[diceResult]
-
-
-    for countT, setPos in enumerate(posList):
+    for countT, setPos in enumerate(pos_list):
 
         if setPos.issubset(set(cardsInPlay)):
-            isTurnImpossible = False
+            is_turn_impossible = False
 
-    return isTurnImpossible
+    return is_turn_impossible
 
-def turnEnding():
 
-    turnScore = 0
+def turn_ending():
+    turn_score = 0
 
-    for count, card in enumerate(cardsInPlay):
-        turnScore += card
+    for countTE1, cardTE1 in enumerate(cardsInPlay):
+        turn_score += cardTE1
 
-    for count, card in enumerate(card_group):
-        card.set_is_used(False)
-        card.set_is_selected(False)
+    for countTE2, cardTE2 in enumerate(card_group):
+        cardTE2.set_is_used(False)
+        cardTE2.set_is_selected(False)
 
     cardsInPlay.clear()
 
-    return turnScore
+    return turn_score
 
-def popMessageBlock():
+
+def pop_message_block():
     pass
 
 
@@ -77,7 +78,7 @@ RED = (255, 0, 0)
 # Constant position
 MSG_POS_XY = (380, 335)
 MSG_BLOCK_XY = (350, 325)
-INFO_BOX_XY = (995,30)
+INFO_BOX_XY = (995, 30)
 
 # Booleans
 isFirstRoll = True
@@ -85,17 +86,16 @@ isDiceRolling = False
 isGameDone = False
 isPlayerTurn = False
 
-#variables
+# variables
 diceResult = 0
-cardsInPlay = [9,8,7,6,5,4,3,2,1]
+cardsInPlay = [9, 8, 7, 6, 5, 4, 3, 2, 1]
 playerScores = []
 
 pygame.init()
 
-#title_font = pygame.font.Font(None, 80)
+# title_font = pygame.font.Font(None, 80)
 msg_font = pygame.font.SysFont("garamond", 28)
 score_font = pygame.font.Font('font/Pixeltype.ttf', 80)
-
 
 # Set the width and height of the screen [width, height]
 size = (1025, 500)
@@ -104,24 +104,24 @@ pygame.display.set_caption('Trac Game')
 bg_surf = pygame.image.load("art/card_table.jpg").convert_alpha()
 bg_surf = pygame.transform.smoothscale(bg_surf, size)
 
-#info button
+# info button
 info_box = pygame.image.load("art/info_box.png").convert_alpha()
-info_box_rect = info_box.get_rect(center = INFO_BOX_XY)
+info_box_rect = info_box.get_rect(center=INFO_BOX_XY)
 
-#Game screen
+# Game screen
 roll_msg = msg_font.render("Press space to roll dices", False, BLACK)
-roll_msg_rect = roll_msg.get_rect(topleft = MSG_POS_XY)
+roll_msg_rect = roll_msg.get_rect(topleft=MSG_POS_XY)
 msg_block = pygame.image.load("art/msg_block.png").convert_alpha()
-msg_block_rect = msg_block.get_rect(topleft = MSG_BLOCK_XY)
+msg_block_rect = msg_block.get_rect(topleft=MSG_BLOCK_XY)
 msg_dice_result = msg_font.render(f"{diceResult}", False, BLACK)
-msg_dice_result_rect = msg_dice_result.get_rect(topleft = MSG_POS_XY)
+msg_dice_result_rect = msg_dice_result.get_rect(topleft=MSG_POS_XY)
 
-#Groups dice
+# Groups dice
 dice_group = pygame.sprite.Group()
 dice_group.add(Dice(1))
 dice_group.add(Dice(2))
 
-#Group player
+# Group player
 player_group = pygame.sprite.Group()
 player_group.add(Player(1))
 playerScores.append(0)
@@ -136,7 +136,7 @@ playerScores.append(0)
 # player_group.add(Player(6))
 # playerScores.append(0)
 
-#9 Cards, create card group sprite
+# 9 Cards, create card group sprite
 card_group = pygame.sprite.Group()
 i = 9
 while i > 0:
@@ -147,7 +147,6 @@ while i > 0:
 clock = pygame.time.Clock()
 
 rollTimer = pygame.USEREVENT + 1
-
 
 listPlayer = iter(player_group)
 currPlayer = next(listPlayer)
@@ -163,7 +162,7 @@ while not isGameDone:
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
             if not isPlayerTurn:
                 if not isDiceRolling:
-                    #player press SPACE to roll dices
+                    # player press SPACE to roll dices
                     if isFirstRoll:
                         isFirstRoll = False
 
@@ -175,7 +174,7 @@ while not isGameDone:
                         dice.roll()
 
         if event.type == rollTimer:
-            #stop rolling dices
+            # stop rolling dices
             isDiceRolling = False
             for dice in dice_group:
                 dice.set_rolling(False)
@@ -183,11 +182,11 @@ while not isGameDone:
 
             # msg_dice_result = msg_font.render(f"{diceResult}", False, BLACK)
 
-            if checkTurnEnd():
+            if check_turn_end():
 
-                currPlayer.add_score(turnEnding())
+                currPlayer.add_score(turn_ending())
                 print(currPlayer.get_score())
-                playerScores[currPlayer.get_player_number() -1] = currPlayer.get_score()
+                playerScores[currPlayer.get_player_number() - 1] = currPlayer.get_score()
                 cardsInPlay = [9, 8, 7, 6, 5, 4, 3, 2, 1]
                 isPlayerTurn = False
                 msg_dice_result = msg_font.render("", False, BLACK)
@@ -232,13 +231,12 @@ while not isGameDone:
             else:
                 msg_dice_result = msg_font.render(f"{diceResult}", False, BLACK)
 
-
-
         if event.type == pygame.MOUSEBUTTONDOWN:
             if not isDiceRolling:
                 if info_box_rect.collidepoint(pygame.mouse.get_pos()):
-                    #put code in method that reset all game information
-                    #isFirstRoll = True
+                    # put code in method that reset all game information
+                    # isFirstRoll = True
+                    # todo info box to complete
                     pass
 
             if isPlayerTurn:
@@ -267,13 +265,10 @@ while not isGameDone:
                     diceResult = 0
                     print(cardsInPlay)
 
-
-
     # --- Drawing code should go here
 
-    #table background
-    screen.blit(bg_surf, (0,0))
-
+    # table background
+    screen.blit(bg_surf, (0, 0))
 
     dice_group.update()
     card_group.update()
@@ -283,19 +278,18 @@ while not isGameDone:
     player_group.draw(screen)
 
     for countS, score in enumerate(playerScores):
-        score_surf = score_font.render(f'{score}',False,(200,200,0))
-        score_rect = score_surf.get_rect(center = (100 + (155 * countS), 85))
-        screen.blit(score_surf,score_rect)
+        score_surf = score_font.render(f'{score}', False, (200, 200, 0))
+        score_rect = score_surf.get_rect(center=(100 + (155 * countS), 85))
+        screen.blit(score_surf, score_rect)
 
     screen.blit(msg_block, msg_block_rect)
-    screen.blit(info_box,info_box_rect)
+    screen.blit(info_box, info_box_rect)
 
     if isFirstRoll:
         screen.blit(roll_msg, roll_msg_rect)
     if diceResult != 0:
-        msg_dice_result_rect = msg_dice_result.get_rect(topleft = MSG_POS_XY)
+        msg_dice_result_rect = msg_dice_result.get_rect(topleft=MSG_POS_XY)
         screen.blit(msg_dice_result, msg_dice_result_rect)
-
 
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
