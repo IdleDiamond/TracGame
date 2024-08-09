abeeccf33ce013951dd5678e321fb0a0abdf4540
IdleDiamond <jfdcoding@gmail.com>
2024-08-09 10:20:19 -0400

    Player.py code finished, need to add new eliminate art

--- lib/player.py
+++ lib/player.py
@@ -16,6 +16,11 @@ PLAYER_Y_POS = 30
 
 
 class Player(pygame.sprite.Sprite):
+
+    ACTIVE = 0
+    INACTIVE = 1
+    ELIMINATED = 3
+
     def __init__(self, player_num):
         super().__init__()
         self.playerNumber = player_num
@@ -26,9 +31,18 @@ class Player(pygame.sprite.Sprite):
         player_active = pygame.image.load(f"art/player/player{self.playerNumber}.png").convert_alpha()
         player_inactive = pygame.image.load(f"art/player/player{self.playerNumber}inactive.png").convert_alpha()
         # todo add pixelart for eliminated
-        self.playerFrame = (player_inactive, player_active)
+        #   player_eliminated = pygame.image.load(f"art/player/player{self.playerNumber}eliminated.png").convert_alpha()
+        self.playerFrame = (player_active, player_inactive)
         self.image = self.playerFrame[1]
         self.rect = self.image.get_rect(midleft=(PLAYER_X_POS + (155 * (self.playerNumber - 1)), PLAYER_Y_POS))
+
+    def status(self):
+        if self.isActivePlayer:
+            return Player.ACTIVE
+        elif self.isEliminated:
+            return Player.ELIMINATED
+        else:
+            return Player.INACTIVE
 
     def get_player_number(self):
         return self.playerNumber
@@ -40,8 +54,8 @@ class Player(pygame.sprite.Sprite):
         return self.isReducedDice
 
     # todo to delete if not used
-    # def getIsActivePlayer(self):
-    #     return self.isActivePlayer
+    #   def getIsActivePlayer(self):
+    #       return self.isActivePlayer
 
     def get_is_eliminated(self):
         return self.isEliminated
@@ -59,5 +73,5 @@ class Player(pygame.sprite.Sprite):
         self.score += score_value
 
     def update(self):
-        self.image = self.playerFrame[self.isActivePlayer]
-        return
+        self.image = self.playerFrame[self.status()]
+        # return
