b15f055ffc0e55c36e4365fd34ade4ee40a54acb
IdleDiamond <jfdcoding@gmail.com>
2024-08-28 12:28:23 -0400

    Created gameCore.py file, will contain game object

---
+++ lib/gameCore.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+ Created on Wed Aug 28 12:25 2024
+
+ @author: joaking
+
+ OOP for game core structure. This class object will be created and used by tracGame.py
+
+"""
+
